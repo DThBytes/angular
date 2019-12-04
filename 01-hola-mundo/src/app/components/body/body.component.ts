c5227a669306acdf5e8abd56db52dd2a36e98ad6
DT <dtheriano@gmail.com>
2019-12-04 20:02:25 -0300

    cap 40

--- 01-hola-mundo/src/app/components/body/body.component.ts
+++ 01-hola-mundo/src/app/components/body/body.component.ts
@@ -6,6 +6,17 @@ import { Component } from '@angular/core';
     templateUrl: `./body.component.html`
 })
 
-export class BodyComponent{
-    
+export class BodyComponent {
+
+    mostrar = true;
+
+    frase: any = {
+        mensaje: 'Un gran poder requiere una gran responsabilidad',
+        autor: 'Ben Parquer'
+    };
+
+    personajes: string[] = ['Spiderman', 'Venom', 'Dr. Octupus'];
+
+
+
 }
