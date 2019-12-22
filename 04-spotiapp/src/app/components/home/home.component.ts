247047c087a01e647f57794d55842a8a1a04e5b9
DT <dtheriano@gmail.com>
2019-12-21 23:09:42 -0300

    Spotiapp

--- 04-spotiapp/src/app/components/home/home.component.ts
+++ 04-spotiapp/src/app/components/home/home.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-home',
@@ -7,7 +8,16 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HomeComponent implements OnInit {
 
-  constructor() { }
+  paises: any[] = [];
+
+  constructor( private http: HttpClient ) {
+    console.log('Constructor del home iniciado');
+    this.http.get('https://restcountries.eu/rest/v2/lang/es')
+      .subscribe( (respuesta: any) => {
+        this.paises = respuesta;
+        console.log(respuesta);
+      } );
+   }
 
   ngOnInit() {
   }
