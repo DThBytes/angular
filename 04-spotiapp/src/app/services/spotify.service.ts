ad004f5a8e79f62d79feb0cc31c258c38c0f0357
DT <dtheriano@gmail.com>
2019-12-22 15:30:07 -0300

    Spotiapp cla 96

--- 04-spotiapp/src/app/services/spotify.service.ts
+++ 04-spotiapp/src/app/services/spotify.service.ts
@@ -11,11 +11,21 @@ export class SpotifyService {
   getNewReleases() {
 
     const headers = new HttpHeaders({
-      Authorization: 'Bearer BQAzcgnTdH2ATZZAhem4uow4fM4arCvRt9Tpzt7CYzQyXKq62neQr8rl-ZjZ8c0WVsPSnfC7_SX2pRIrXlQ'
+      // tslint:disable-next-line: max-line-length
+      Authorization: 'Bearer BQDaOwmI5ZotlM52gR-TtPFhAl6a0KJHiIRQSFPu1JStkguGWJOoGsdA3gWG_tnyqLYbO69y6P673E20mftkh8aXg2941KBbPb1Ft10E1xFpseq0jEgUHXNrt0JXJ-7OfISBm37jC9wH'
     });
 
     return this.http.get('https://api.spotify.com/v1/browse/new-releases', { headers });
 
+  }
+
+  getArtista( termino: string ) {
+    const headers = new HttpHeaders({
+      // tslint:disable-next-line: max-line-length
+      Authorization: 'Bearer BQDaOwmI5ZotlM52gR-TtPFhAl6a0KJHiIRQSFPu1JStkguGWJOoGsdA3gWG_tnyqLYbO69y6P673E20mftkh8aXg2941KBbPb1Ft10E1xFpseq0jEgUHXNrt0JXJ-7OfISBm37jC9wH'
+    });
+
+    return this.http.get(`https://api.spotify.com/v1/search?q=${ termino }&type=artist&limit=15`, { headers });
   }
 
 }
