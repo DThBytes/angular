9b588d7285611ee59965d2cecd87c8de417bf439
DT <dtheriano@gmail.com>
2019-12-21 21:06:35 -0300

    Spotiapp

--- 04-spotiapp/src/app/app.module.ts
+++ 04-spotiapp/src/app/app.module.ts
@@ -2,7 +2,6 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
-<<<<<<< HEAD
 import { HomeComponent } from './components/home/home.component';
 import { SearchComponent } from './components/search/search.component';
 import { ArtistaComponent } from './components/artista/artista.component';
@@ -15,12 +14,6 @@ import { NavbarComponent } from './components/shared/navbar/navbar.component';
     SearchComponent,
     ArtistaComponent,
     NavbarComponent
-=======
-
-@NgModule({
-  declarations: [
-    AppComponent
->>>>>>> 5701beca4cdc16233a32b4fe731780765d05c3c0
   ],
   imports: [
     BrowserModule
